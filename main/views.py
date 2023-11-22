6275794b78287f9673d01fd2bd4c5bd30e9dc644
prasetyoadii <prasetyo.adi21@ui.ac.id>
2023-11-22 10:26:15 +0700

    Mengerjakan bonus

--- main/views.py
+++ main/views.py
@@ -79,6 +79,7 @@ def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@csrf_exempt
 def get_item_json(request):
     item = Item.objects.filter(user = request.user)
     return HttpResponse(serializers.serialize('json', item))
